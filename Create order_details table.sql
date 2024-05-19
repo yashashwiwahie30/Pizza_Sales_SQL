e1381bd86034f05c0f0257053cfd81a10b70d5ee
Yashashwi Wahie <103446037+yashashwiwahie30@users.noreply.github.com>
2024-05-19 13:07:14 +0530

    Delete Create order_details table.sql

--- Create order_details table.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM pizzahut.order_details;
-
-create table order_details (
-order_details_id int not null,
-order_id int not null,
-pizza_id text not null,
-quantity int not null, 
-primary key(order_details_id) );
-
